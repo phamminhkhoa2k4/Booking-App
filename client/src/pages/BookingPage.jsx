c917c355389a97887d2def7ff41a373ac54236f2
pham minh khoa <khoapham280@gmail.com>
2024-04-24 01:06:00 +0700

    ✨ 💄 style and feat: style single booking page and display

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,10 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import AddressLink from "../components/Address Link/AddressLink";
+import PlaceGallery from "../components/Place Gallery/PlaceGalllery";
+import BookingDates from "../components/Booking Dates/BookingDates";
 
 const BookingPage = () => {
-  const { id } = useParams;
+  const { id } = useParams();
+  const [booking, setBooking] = useState(null);
+  useEffect(() => {
+    if (id) {
+      axios.get("/bookings").then((res) => {
+        const foundBooking = res.data.find(({ _id }) => _id === id);
+        if (foundBooking) {
+          setBooking(foundBooking);
+        }
+      });
+    }
+  }, [id]);
+  if (!booking) return "";
   return (
-    <div className="mt-5">
-      <h1>{id}</h1>
+    <div className="my-8">
+      <h1 className="text-3xl">{booking.place.title}</h1>
+      <AddressLink>{booking.place.address}</AddressLink>
+      <div className="flex justify-between bg-gray-200 p-6 my-6 rounded-2xl">
+        <div>
+          <h2 className="text-2xl mb-4">Your booking information</h2>
+          <BookingDates booking={booking} />
+        </div>
+        <div className="bg-primary p-6 text-white rounded-2xl">
+          <div>Total Price</div>
+          <div className="text-3xl">${booking.price}</div>
+        </div>
+      </div>
+      <PlaceGallery place={booking.place} />
     </div>
   );
 };
